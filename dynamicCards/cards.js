b9243b534c2bd8b02da82bf96ce727278cd17ad5
complikatyed <katye.russell@gmail.com>
2016-03-05 11:56:59 -0600

    Add makeCard and eraseCard functions - currently erasing all cards

--- dynamicCards/cards.js
+++ dynamicCards/cards.js
@@ -1,6 +1,29 @@
 var createButton = document.getElementById("createBtn");
+var cardInput = document.getElementById("createTxt");
+var cardEl = document.getElementById("cardArea");
+var deleteButton = document.getElementById("deleteBtn");
 
-
+// Event listener for the create button
 createButton.addEventListener("click", function() {
   console.log("You clicked me!");
+  makeCard();
+
 });
+
+// Each card needs to include:
+  // Class of "card"
+  // User inputted text
+  // A delete button
+
+var makeCard = function() {
+  cardEl.innerHTML += "<article class='card'>" + cardInput.value + "<br><button id='deleteBtn'>Delete</button></article>";
+};
+
+var eraseCard = function() {
+  cardEl.innerHTML = "";
+}
+
+cardEl.addEventListener("click", function(){
+  eraseCard();
+});
+
